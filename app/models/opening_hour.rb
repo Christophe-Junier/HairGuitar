546a3b75ac5383c4ede43a9e8f97081576d34ea8
Christophe JUNIER <christophejunier@gmail.com>
2020-09-21 10:41:30 +0200

    adding basic relationship on model

--- app/models/opening_hour.rb
+++ app/models/opening_hour.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class OpeningHour < ApplicationRecord
+  belongs_to :pro
 end
--- app/models/pro.rb
+++ app/models/pro.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Pro < ApplicationRecord
+  has_many :appointments
+  has_many :opening_hours
 end
