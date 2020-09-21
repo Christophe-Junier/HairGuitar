c27caf12847e07d664b9265f7e6b32e9c86a4a39
Christophe JUNIER <christophejunier@gmail.com>
2020-09-21 10:06:39 +0200

    adding opening_hour model, migration and test

---
+++ db/migrate/20200921080607_create_opening_hours.rb
@@ -0,0 +1,11 @@
+class CreateOpeningHours < ActiveRecord::Migration[6.0]
+  def change
+    create_table :opening_hours do |t|
+      t.string :day
+      t.time :starts_at
+      t.time :ends_at
+
+      t.timestamps
+    end
+  end
+end
