bee67c4037c4293506b18dadbee965e4bbf4eb3c
Christophe JUNIER <christophejunier@gmail.com>
2020-09-22 16:09:49 +0200

    adding html form and controller action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,18 @@
 class BookingsController < ApplicationController
 
-  def new; end
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(bookings_params)
+    @booking.prestations << Prestation.where(id: params[:booking][:prestation_ids])
+    redirect_to new_booking_pro_path(booking: @booking)
+  end
+
+  private
+
+  def bookings_params
+    params.require(:booking).permit(:email, :name, :address)
+  end
 end
