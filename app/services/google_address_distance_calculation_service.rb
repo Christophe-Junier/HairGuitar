162c322a66cde848efe381ed14f7b17ad65809f7
Christophe JUNIER <christophejunier@gmail.com>
2020-09-23 18:11:50 +0200

    linting with rubocop

--- app/services/google_address_distance_calculation_service.rb
+++ app/services/google_address_distance_calculation_service.rb
@@ -2,14 +2,14 @@
 
 # Service to generate lng and lat with an address using google API
 class GoogleAddressDistanceCalculationService
+  # rubocop:disable Metrics/AbcSize
   def get_pro_booking_distance(pro, booking)
-      client = HTTPClient.new
-      uri = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' \
-            + pro.address.gsub(' ', '+') \
-            + '&destinations=' \
-            + booking.address.gsub(' ', '+')
-      api_key = ENV['GOOGLE_API_KEY']
-      structured_response = JSON.parse(client.get_content("#{uri}&key=#{api_key}"))
-      structured_response['rows'].first['elements'].first['distance']['text'].split(' ').first
+    client = HTTPClient.new
+    uri = 'https://maps.googleapis.com/maps/api/distancematrix/json'
+    params = "?origins=#{pro.address.gsub(' ', '+')}&destinations=#{booking.address.gsub(' ', '+')}"
+    api_key = ENV['GOOGLE_API_KEY']
+    structured_response = JSON.parse(client.get_content("#{uri + params}&key=#{api_key}"))
+    structured_response['rows'].first['elements'].first['distance']['text'].split(' ').first
   end
+  # rubocop:enable Metrics/AbcSize
 end
