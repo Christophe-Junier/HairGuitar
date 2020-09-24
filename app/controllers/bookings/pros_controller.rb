0fdaaadaa0aa61af7836f4f8d8ad3ebd21c93af8
Christophe JUNIER <christophejunier@gmail.com>
2020-09-24 09:41:24 +0200

    adding available pro booking service and refactoring booking pro controller

--- app/controllers/bookings/pros_controller.rb
+++ app/controllers/bookings/pros_controller.rb
@@ -3,47 +3,10 @@
 module Bookings
   # Controller to choose available pros for a booking.
   class ProsController < ApplicationController
-    # rubocop:disable Metrics/MethodLength
-    # rubocop:disable Metrics/AbcSize
     def new
       @booking = Booking.find(params[:booking_id])
-      booking_end_time = @booking.starts_at + @booking.prestations.pluck(:duration).sum.minutes
-
-      # Pros that can are open at booking time
-      # Problems on time type on postgres that always add a date on time type column
-      # Solving by forcing the same date on time comparison
-      open_pros = Pro.joins(:opening_hours)
-                     .where('opening_hours.day = ? AND opening_hours.starts_at < ? AND opening_hours.ends_at > ?',
-                            @booking.starts_at.strftime('%A').downcase,
-                            @booking.starts_at.strftime('Sat, 01 Jan 2000 %H:%M:%S UTC +00:00').to_time,
-                            @booking.starts_at.strftime('Sat, 01 Jan 2000 %H:%M:%S UTC +00:00').to_time)
-
-      # Pros that can dont have crossing appointments
-      no_crossing_appointment_pros = open_pros.joins(:appointments)
-                                              .where.not('appointments.starts_at < ? AND appointments.ends_at > ?
-                                                         AND appointments.starts_at < ? AND appointments.ends_at > ?',
-                                                         @booking.starts_at, @booking.starts_at,
-                                                         booking_end_time, booking_end_time)
-
-      # Pros that can handle booking prestations
-      available_for_prestations_pros = no_crossing_appointment_pros.joins(:prestations)
-                                                                   .where.not(
-                                                                     prestations: {
-                                                                       id: Prestation.pluck(:id) - \
-                                                                           @booking.prestations.ids
-                                                                     }
-                                                                   ).uniq
-
-      # Pros with max distance that are within booking distance
-      available_for_prestations_pros.each_with_index do |pro, index|
-        address_distance_service = GoogleAddressDistanceCalculationService.new
-        distance = address_distance_service.get_pro_booking_distance(pro, @booking).to_i
-        available_for_prestations_pros.delete_at(index) unless distance <= pro.max_kilometers
-      end
-      @pros = available_for_prestations_pros
+      @pros = AvailableProForBookingService.new(@booking).process_all
     end
-    # rubocop:enable Metrics/MethodLength
-    # rubocop:enable Metrics/AbcSize
 
     def create
       @booking = Booking.find(params[:booking_id])
---
+++ app/services/available_pro_for_booking_service.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+# Service to return available pro for a booking
+class AvailableProForBookingService
+  def initialize(booking)
+    @booking = booking
+    @booking_end_time = @booking.starts_at + \
+                        @booking.prestations.pluck(:duration).sum.minutes
+  end
+
+  # Processing all methods to get the available pro list for a booking
+  def process_all
+    @pros = open_pros()
+    @pros = no_crossing_appointment_pros()
+    @pros = available_for_prestations_pros()
+    @pros = avaible_at_booking_distance_pro()
+    @pros
+  end
+
+  # Pros that can are open at booking time
+  # Problems on time type on postgres that always add a date on time type column
+  # Solving by forcing the same date on time comparison
+  def open_pros
+    Pro.joins(:opening_hours)
+       .where('opening_hours.day = ? AND opening_hours.starts_at < ? AND opening_hours.ends_at > ?',
+              @booking.starts_at.strftime('%A').downcase,
+              @booking.starts_at.strftime('Sat, 01 Jan 2000 %H:%M:%S UTC +00:00').to_time,
+              @booking.starts_at.strftime('Sat, 01 Jan 2000 %H:%M:%S UTC +00:00').to_time)
+  end
+
+  # Pros that can dont have crossing appointments
+  def no_crossing_appointment_pros
+    @pros.joins(:appointments)
+         .where.not('appointments.starts_at < ? AND appointments.ends_at > ?
+                    AND appointments.starts_at < ? AND appointments.ends_at > ?',
+                    @booking.starts_at, @booking.starts_at,
+                    @booking_end_time, @booking_end_time)
+  end
+
+  # Pros that can handle booking prestations
+  def available_for_prestations_pros
+    @pros.joins(:prestations)
+         .where.not(
+           prestations: {
+             id: Prestation.pluck(:id) - \
+                 @booking.prestations.ids
+           }
+         ).uniq
+  end
+
+  # Pros with max distance that are within booking distance
+  def avaible_at_booking_distance_pro
+    @pros.each_with_index do |pro, index|
+      address_distance_service = GoogleAddressDistanceCalculationService.new
+      distance = address_distance_service.get_pro_booking_distance(pro, @booking).to_i
+      @pros.delete_at(index) unless distance <= pro.max_kilometers
+    end
+    @pros = available_for_prestations_pros
+  end
+end
